c68ef635225b26ec8405345e9144408fb0e21e86
Pkshetlie <pierrick.pobelle@gmail.com>
2020-12-16 16:18:59 +0100

    mise à jour avec exemple qui en marche pas.

--- MapInfoMixin.cs
+++ MapInfoMixin.cs
@@ -40,16 +40,31 @@ namespace TwitchIntegration
 
         public override void OnRefresh()
         {
-            
-               Campaign.Current.ConversationManager.OnConversationActivate();
+
+            // Dans l'idée il faudra récupéréer les options et la conversation avant qu'elle soit affichée.
+           // Campaign.Current.ConversationManager.ConversationBegin; semble etre une des clé de ce problème mais je n'ai aucune connaissanence en events .. :/ 
 
 
-            var x = Campaign.Current.ConversationManager;
-           
+           // ci-dessous ne fonctionne pas, les options ne se remplacent pas dans le texte dans l'idée il faudrait pouvoir modifier le texte toutes 
+            // les X secondes en fonction de ce que les viewwers auront dit dans le tchat.
+           var CurrentConversation = Campaign.Current.ConversationManager;
+            if (CurrentConversation.CurOptions != null)
+            {
+                var options = new List<ConversationSentenceOption>(CurrentConversation.CurOptions);
 
-           
-
+                if (options != null && options.Count > 0)
+                {
+                    CurrentConversation.ClearCurrentOptions();
+                    foreach (var option in options)
+                    {
+                        var textEdited = new TextObject("Test insertion " + option.Text.ToString());
+                        //var hintTextEdited = new TextObject("Test insertion hint " + option.HintText.ToString());
+                        Campaign.Current.ConversationManager.AddToCurrentOptions(textEdited, option.Id, option.IsClickable, option.HintText);
+                    }
+                }
+            }
         }
+
 
     }
 }
